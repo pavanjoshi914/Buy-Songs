3985772364b37aed41090bf735782895187d89c5
Pavanjoshi914 <pavanj914@gmail.com>
2022-04-17 01:53:01 +0530

    initial prototype for adding metadata in transactions

--- src/components/BuySong.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function BuySong() {
-  return (
-    <div><button className="glow-on-hover position-button">Buy This Song</button></div>
-  )
-}
-
-export default BuySong
